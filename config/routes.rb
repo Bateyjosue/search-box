5fc7fb6920642808e6335dc07b61db3c5eeacd2b
Bateyjosue <josuebatey19@gmail.com>
2023-05-04 18:37:08 +0200

    Add Article with title and content field

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "home#index"
+  root "articles#index"
 end
